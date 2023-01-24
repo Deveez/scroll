abe9f1c18b027eb464855d274de2e204cf944f59
William Balance <williambalance33@icloud.com>
2023-01-24 10:27:20 +0100

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <div style={{ height: "100vh" }}>
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       <div>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat volutpat quam, eget bibendum leo. Praesent
         lacinia purus non turpis suscipit, nec bibendum erat sagittis. Suspendisse rhoncus sagittis sapien nec mattis.
@@ -13,42 +13,58 @@ function App() {
         aliquam mauris, eget interdum dolor justo imperdiet metus. Nam facilisis, enim id ultrices sollicitudin, quam
         velit elementum augue, id elementum augue sem sit amet enim.
       </div>
-      <div>
-        {/* Uniquement scroll sur ça */}
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat volutpat quam, eget bibendum leo. Praesent
-        lacinia purus non turpis suscipit, nec bibendum erat sagittis. Suspendisse rhoncus sagittis sapien nec mattis.
-        Cras efficitur condimentum quam, nec malesuada velit interdum dapibus. Phasellus aliquam suscipit varius. Nunc
-        risus purus, fermentum sed massa in, venenatis hendrerit eros. Sed quis nisi in risus tempus fringilla. Aliquam
-        laoreet erat at nunc consectetur dignissim. Nam tortor nibh, tempor nec rhoncus quis, lacinia a mauris. Donec
-        vitae urna faucibus, mattis orci quis, eleifend mauris. Fusce ullamcorper, enim eu blandit auctor, neque ipsum
-        aliquam mauris, eget interdum dolor justo imperdiet metus. Nam facilisis, enim id ultrices sollicitudin, quam
-        velit elementum augue, id elementum augue sem sit amet enim.
+      <div style={{ display: "flex" }}>
+        <div>
+          <div style={{ overflowY: "auto" }}>
+            {/* Uniquement scroll sur ça */}
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat volutpat quam, eget bibendum leo.
+            Praesent lacinia purus non turpis suscipit, nec bibendum erat sagittis. Suspendisse rhoncus sagittis sapien
+            nec mattis. Cras efficitur condimentum quam, nec malesuada velit interdum dapibus. Phasellus aliquam
+            suscipit varius. Nunc risus purus, fermentum sed massa in, venenatis hendrerit eros. Sed quis nisi in risus
+            tempus fringilla. Aliquam laoreet erat at nunc consectetur dignissim. Nam tortor nibh, tempor nec rhoncus
+            quis, lacinia a mauris. Donec vitae urna faucibus, mattis orci quis, eleifend mauris. Fusce ullamcorper,
+            enim eu blandit auctor, neque ipsum aliquam mauris, eget interdum dolor justo imperdiet metus. Nam
+            facilisis, enim id ultrices sollicitudin, quam velit elementum augue, id elementum augue sem sit amet enim.
+          </div>
+        </div>
+        <div>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat volutpat quam, eget bibendum leo.
+            Praesent lacinia purus non turpis suscipit, nec bibendum erat sagittis. Suspendisse rhoncus sagittis sapien
+            nec mattis. Cras efficitur condimentum quam, nec malesuada velit interdum dapibus. Phasellus aliquam
+            suscipit varius. Nunc risus purus, fermentum sed massa in, venenatis hendrerit eros. Sed quis nisi in risus
+            tempus fringilla. Aliquam laoreet erat at nunc consectetur dignissim. Nam tortor nibh, tempor nec rhoncus
+            quis, lacinia a mauris. Donec vitae urna faucibus, mattis orci quis, eleifend mauris. Fusce ullamcorper,
+            enim eu blandit auctor, neque ipsum aliquam mauris, eget interdum dolor justo imperdiet metus. Nam
+            facilisis, enim id ultrices sollicitudin, quam velit elementum augue, id elementum augue sem sit amet enim.
+          </div>
+        </div>
       </div>
     </div>
   );
